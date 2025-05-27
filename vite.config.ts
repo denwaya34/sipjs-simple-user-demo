f83ca5c0cc8c1aef2d5e1ec57612099ba19ad439
denwaya34 <denwaya34@gmail.com>
2025-05-27 10:16:30 +0900

    init

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import { checker } from 'vite-plugin-checker';
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    open: true,
+    proxy: {
+      '/api/': {
+        target: 'https://test.u.biztel.jp:8000',
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    checker({
+      typescript: true,
+    }),
+  ],
+});
